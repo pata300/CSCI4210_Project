c60b6a9dba87113f237cd35a17562f07ddefd634
Patrick Griffin <pgriffi1@uno.edu>
2020-02-27 18:08:21 -0600

    algorithm work

---
+++ Question.js
@@ -0,0 +1,20 @@
+//question object
+    //String question
+    //String correctAnswer
+    //String[] falseAnswer
+    
+    //setQuestion(String question)
+    
+    //getQuestion()
+        //return question
+    
+    //setCorrectAnswer(String)
+    //could set all correct answers to the same index in the falseAnswer array?
+
+    //getCorrectAnswer()
+        //return correctAnswer
+
+    //setFalseAnswer(String)
+
+    //getFalseAnswer()
+        //return falseAnswer
---
+++ QuizMaker.js
@@ -0,0 +1,27 @@
+// Quiz Maker Algorithm 1.0
+
+//create an new question object
+//Question testQuestion = new Question();
+
+// *Submission of questions*
+// User clicks button to add question
+    //eventListener with text box is connected to button
+
+// which ask user for question
+    // let question = text box string
+// 	User submits question
+    //testQueston.setQuestion(question);
+
+// Ask user for answer to question
+// 	User submits correct answer
+    //testQuestion
+// Ask user for 3 false answers
+// 	User submits 3 false answers
+ 
+// JSON file stores questions
+ 
+// *Generate quiz button*
+	
+// QuizMaker populates page with questions
+
+// Answer key
