9c158f38d6fc70eca8bdc00ec628a7b842822110
Patrick Griffin <pgriffi1@uno.edu>
2020-03-04 22:46:08 -0600

    QuizMaker modified

--- QuizMaker.js
+++ QuizMaker.js
@@ -1,27 +1,67 @@
 // Quiz Maker Algorithm 1.0
 
-//create an new question object
-//Question testQuestion = new Question();
+//variables to declare
+/*
+addBtn will request a text box to add a question
+submitBtn will submit quetion (call setQuestion(question))
+addBtn.listener call text box
+submitBtn.listner call setQuestion(question)
 
-// *Submission of questions*
-// User clicks button to add question
-    //eventListener with text box is connected to button
+create an new question object
+Question testQuestion = new Question();
 
-// which ask user for question
-    // let question = text box string
-// 	User submits question
-    //testQueston.setQuestion(question);
 
-// Ask user for answer to question
-// 	User submits correct answer
-    //testQuestion
-// Ask user for 3 false answers
-// 	User submits 3 false answers
+*Submission of questions*
+User clicks button to add question
+    eventListener with text box is connected to button
+    
+which ask user for question
+    let question = questionText id from HTML
+
+user clicks submit button
+	User submits question
+    testQueston.setQuestion(question);
+
+Ask user for answer to question
+	User submits correct answer
+    testQuestion
+Ask user for 3 false answers
+    User submits 3 false answers
+    
+print question with answers in a form on page
+    allow to edit or delete entry   
  
-// JSON file stores questions
+MySQL stores questions
  
-// *Generate quiz button*
+*Generate quiz button*
 	
-// QuizMaker populates page with questions
+QuizMaker populates page with questions
 
-// Answer key
+Answer key
+
+*/
+
+let addQuestionBtn = document.getElementById('addBtn');
+let submitBtn = document.getElementById('submitBtn');
+let newQueston = document.getElementById('question'); //text from question text box
+let correctBtn = document.getElementById('correctBtn');
+
+Question testQuestion = new Question();
+
+submitBtn.addEventListener('submit', addQuestion);
+correctBtn.addEventListener('submit', addCorrectAnswer);
+
+function addQuestion(e){
+    testQuestion.setQuestion(e);
+    addCorrectAnswer();
+}
+
+function addCorrectAnswer(e){
+    //variable for the input
+    let answer = document.getElementById('item').value;
+
+    //create correct answer box/button
+
+
+    testQuestion.setCorrectAnswer(answer);
+}
